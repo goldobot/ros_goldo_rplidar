f0596bfc029bcd00b5e216edeee9614b2849e40c
Thomas Liennard <thomas.liennard@valeo.com>
2020-02-13 16:11:15 +0100

    wip

--- src/goldo_rplidar_node.cpp
+++ src/goldo_rplidar_node.cpp
@@ -3,10 +3,24 @@
 
 #include "rplidar.h"
 #include "std_msgs/String.h"
+#include "goldo_rplidar/StartMotor.h"
+#include "goldo_rplidar/StopMotor.h"
 
 using namespace rp::standalone::rplidar;
 
 RPlidarDriver * g_driver = nullptr;
+
+bool startMotorCallback(goldo_rplidar::StartMotor::Request& request, goldo_rplidar::StartMotor::Response& response)
+{
+	g_driver->startMotor();
+	return true;
+};
+
+bool stopMotorCallback(goldo_rplidar::StopMotor::Request& request, goldo_rplidar::StopMotor::Response& response)
+{
+	g_driver->stopMotor();
+	return true;
+};
 
 int main(int argc, char **argv)
 {
@@ -17,18 +31,20 @@ int main(int argc, char **argv)
 	
   ros::init(argc, argv, "goldo_rplidar");
 
-  ros::NodeHandle n("rplidar");
+  ros::NodeHandle n;
 
   //ros::Publisher chatter_pub = n.advertise<goldo_comm_uart::RawMessage>("stm32/out/raw", 1000);
   //ros::Subscriber raw_message_sub = n.subscribe<goldo_comm_uart::RawMessage>("stm32/in/raw", 1000, chatterCallback);
 
+  auto start_motor_srv = n.advertiseService("start_motor", startMotorCallback);
+  auto stop_motor_srv = n.advertiseService("stop_motor", stopMotorCallback);
   
   ros::Rate loop_rate(1000);
   
   std::string port_name;
   int baudrate;
-  n.getParam("port", port_name);
-  n.getParam("baudrate", baudrate);
+  ros::param::get("~port", port_name);
+  ros::param::get("~baudrate", baudrate);
 
 
   if(IS_OK(g_driver->connect(port_name.c_str(), 115200)))
@@ -36,7 +52,7 @@ int main(int argc, char **argv)
   } else
   {
 	  ROS_INFO_STREAM("Failed to connect to RPLidar on port: " << port_name);
-	  return -1;
+	  //return -1;
   }
   
   while (ros::ok())
